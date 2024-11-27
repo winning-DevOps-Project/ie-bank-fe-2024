caefd88ce14a7d1e1785804ca77933de96c2850a
ramcav <110005362+ramcav@users.noreply.github.com>
2024-11-27 11:00:40 +0100

    Fixing urls

--- src/utils/getUrl.js
+++ src/utils/getUrl.js
@@ -3,6 +3,5 @@
 export function getApiUrl(path) {
     const baseUrl = process.env.VUE_APP_API_URL;
     const isLocal = process.env.NODE_ENV === "local";
-    return isLocal ? `${baseUrl}${path}/` : `${baseUrl}${path}`;
-  }
-  
+    return `${baseUrl}${path}/`
+}
